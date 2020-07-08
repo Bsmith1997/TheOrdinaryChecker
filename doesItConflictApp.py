86c0972e13382868cf11b96387bf0c364b75c03a
Babette Smith <babette.smith@mail.mcgill.ca>
2020-07-08 17:51:59 -0400

    Added error checking

--- doesItConflictApp.py
+++ doesItConflictApp.py
@@ -19,15 +19,18 @@ def main():
     for row in range(sheet.nrows):
         if sheet.cell(row,product_col).value == toCheck:
             group = sheet.cell(row,group_col).value
-            lbl2 = Label(f2, font='Helvetica 14 bold',text = toCheck + " conficts with: ")
+            lbl2 = Label(f2, font='Helvetica 14 bold',text = toCheck + " conficts with: ", background="coral1")
             lbl2.place(anchor="c",relx=.5, rely=.4)
             for rows in range(sheet.nrows):
                 if group in sheet.row_values(rows)[1]:
-                    x.append(sheet.row_values(rows)[0])
+                    if sheet.row_values(rows)[0] != toCheck and sheet.row_values(rows)[0] != "Product":
+                        x.append(sheet.row_values(rows)[0])
                 if toCheck in sheet.row_values(rows)[1]:
-                    y.append(sheet.row_values(rows)[0])
+                    if sheet.row_values(rows)[0] != toCheck and sheet.row_values(rows)[0] != "Product":
+                        y.append(sheet.row_values(rows)[0])
                 if sheet.row_values(rows)[0] in sheet.cell(row,1).value:
-                    z.append(sheet.row_values(rows)[0])
+                    if sheet.row_values(rows)[0] != toCheck and sheet.row_values(rows)[0] != "Product":
+                        z.append(sheet.row_values(rows)[0])
     x_set = set(x)
     y_set = set(y)
     z_set = set(z)
@@ -35,14 +38,18 @@ def main():
     total = x + y + z
     total_set = set(total)
    
-    final = "\n ".join(str(e) for e in total_set)
-    lbl4 = Label(f2, text=final)
-    lbl4.place(anchor="c",relx=.5, rely=.5)
+    if not total_set:
+        lbl4 = Label(f2, text="This product does not have any conficts. \n If this does not make sense then please check your spelling, \n this is case and spelling sensitive!", background="coral1", font='Helvetica 14 bold')
+        lbl4.place(anchor="c",relx=.5, rely=.7)
+    else: 
+        final = "\n ".join(str(e) for e in total_set)
+        lbl4 = Label(f2, text=final, background="coral1")
+        lbl4.place(anchor="c",relx=.5, rely=.7)
 
 window = Tk()
 window.geometry("500x500")
 f1 = Frame(width=800, height=600, background="white")
-f2 = Frame(width=775, height=575, background="white")
+f2 = Frame(width=775, height=575, background="SkyBlue3")
 f1.pack(fill="both", expand=True, padx=20, pady=2)
 f2.place(in_=f1, anchor="c", relx=.5, rely=.5)
 
@@ -51,7 +58,7 @@ f2.place(in_=f1, anchor="c", relx=.5, rely=.5)
 
 window.title("Does it confict?")
 
-lbl = Label(f2, text="Enter the name of the product to check conficts:")
+lbl = Label(f2, text="Enter the name of the product to check conficts:", background="SkyBlue3", font='Helvetica 14 bold')
 lbl.place(anchor="c",relx=.5, rely=.2)
 #lbl.grid(column=5, row=0)
 
@@ -60,7 +67,7 @@ txt = Entry(f2,width=10)
 txt.place(anchor="c",relx=.5, rely=.25)
 txt.focus()
 
-btn = Button(f2, text="Check it!", command = main)
+btn = Button(f2, text="Check it!", command = main, bg="SkyBlue4")
 
 btn.place(anchor="c",relx=.5, rely=.3)
 
